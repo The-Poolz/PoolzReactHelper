dbdfc1014e8d55fe01d7c49eba624ca0c2ff57f4
DVitaliy <dynnik.vitaliy@gmail.com>
2023-02-08 17:09:52 +0200

    Added tests

---
+++ src/__tests__/ThePoolz.test.ts
@@ -0,0 +1,10 @@
+import ThePoolz from "../poolz/ThePoolz"
+
+describe("ThePoolz", () => {
+  const thePoolz = new ThePoolz(null)
+  test("thePoolz instanse", async () => {
+    expect(thePoolz).toBeDefined()
+    expect(typeof thePoolz.init).toBe("function")
+    expect(await thePoolz.init()).toBeInstanceOf(ThePoolz)
+  })
+})
