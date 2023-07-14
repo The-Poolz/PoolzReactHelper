e2e10650b5a553ae9bf0aed2cb933d1c1c866b70
DVitaliy <dynnik.vitaliy@gmail.com>
2023-07-14 16:10:29 +0300

    Added signInWithEthereum hook

---
+++ src/hooks/useSiwe.ts
@@ -0,0 +1,47 @@
+import { useMemo } from "react"
+import { useThePoolz } from "./useThePoolz"
+
+interface ISiwe {
+  Domain: string
+  Statement: string
+  Version: number
+  ChainId: number
+  Nonce: number
+  IssuedAt: string
+}
+
+export const useSiwe = ({ Domain, Statement, Version, ChainId, Nonce, IssuedAt }: Partial<ISiwe>) => {
+  const thePoolz = useThePoolz()
+  const { web3, account } = thePoolz
+
+  const { host: domain } = window.location
+  const MessageTemplate = `${Domain ?? domain} wants you to sign in with your Ethereum account:
+${account}
+
+${Statement ?? "I accept the Poolz Terms & Conditions: https://www.poolz.finance/terms-conditions"}
+
+URI: ${Domain ?? domain}
+Version: ${Version ?? 1}
+Chain ID: ${ChainId ?? 56}
+Nonce: ${Nonce ?? new Date().getTime()}
+Issued At: ${IssuedAt ?? new Date().toISOString()}
+Expiration Time: ${new Date(new Date().getTime() + 1000 * 60 * 60 * 24).toISOString()}`
+
+  return useMemo(() => {
+    const signInWithEthereum = async () => {
+      if (!web3.currentProvider || !account) return
+      try {
+        // @ts-ignore
+        const signature = (await web3.currentProvider.request({
+          method: "personal_sign",
+          params: [MessageTemplate.replace(/[\r\n]/g, "\n"), account]
+        })) as string
+
+        return { MessageTemplate, signature, formatedMessage: MessageTemplate.replace(/\n/g, "\\n") }
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    return { signInWithEthereum }
+  }, [web3, MessageTemplate, account])
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import ThePoolzProvider from "./components/Provider"
 import { useThePoolz } from "./hooks/useThePoolz"
 import { useConnectWallet } from "./hooks/useConnectWallet"
+import { useSiwe } from "./hooks/useSiwe"
 
-export { ThePoolzProvider, useThePoolz, useConnectWallet }
+export { ThePoolzProvider, useThePoolz, useConnectWallet, useSiwe }
 export type { IThePoolzInterface, IERC20Info } from "./types/IThePoolzInterface"
 export * from "./utils"
 export * from "./constants"
