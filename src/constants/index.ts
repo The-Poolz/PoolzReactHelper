3153bdaacce0d0accc05b2465899dfad336ee547
DVitaliy <dynnik.vitaliy@gmail.com>
2023-09-14 16:16:15 +0300

    Added new chain

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,6 +5,7 @@ import { AvalancheChainConfig } from "../contracts/configs/Avalanche.config"
 import { BinanceTestChainConfig } from "../contracts/configs/BinanceTest.config"
 import { BinanceMainChainConfig } from "../contracts/configs/BinanceMain.config"
 import { MaticMainChainConfig } from "../contracts/configs/MaticMain.config"
+import { NeonChainConfig } from "../contracts/configs/Neon.config"
 
 export const DEFAULT_CHAIN_ID = 1
 export const UINT_MINUS_ONE = "115792089237316195423570985008687907853269984665640564039457584007913129639935"
@@ -16,5 +17,6 @@ AVAILABLE_CHAINS.set(97, BinanceTestChainConfig)
 AVAILABLE_CHAINS.set(137, MaticMainChainConfig)
 AVAILABLE_CHAINS.set(42161, ArbitrumOneChainConfig)
 AVAILABLE_CHAINS.set(43114, AvalancheChainConfig)
+AVAILABLE_CHAINS.set(245022934, NeonChainConfig)
 
 export const CUSTOMER_ACCOUNT_VARIABLE = "__CUSTOMER_ACCOUNT__"
---
+++ src/contracts/configs/README.md
@@ -0,0 +1,1 @@
+The chain number located in "constants/index"
