f62a833f4f87556d1153b049abbc0925d38dfec5
DVitaliy <dynnik.vitaliy@gmail.com>
2023-10-02 22:34:57 +0300

    Issue #18

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import ThePoolzProvider from "./components/Provider"
 import { useThePoolz } from "./hooks/useThePoolz"
 import { useConnectWallet } from "./hooks/useConnectWallet"
 import { useSiwe } from "./hooks/useSiwe"
+import { useSidNameForAddress } from "./hooks/useSid"
 
-export { ThePoolzProvider, useThePoolz, useConnectWallet, useSiwe }
+export { ThePoolzProvider, useThePoolz, useConnectWallet, useSiwe, useSidNameForAddress }
 export type { IThePoolzInterface, IERC20Info } from "./types/IThePoolzInterface"
 export * from "./utils"
 export * from "./constants"
