72bff7df6275446aa576affc8fbfc28d58dc3a01
Ashwin Arora <ashwinarora48@gmail.com>
2023-11-02 23:52:37 +0530

    setup all LDNFT related contracts

--- src/contracts/configs/BinanceTest.config.ts
+++ src/contracts/configs/BinanceTest.config.ts
@@ -17,5 +17,37 @@ export const BinanceTestChainConfig: TChainConfig = {
   lockDealNFT: {
     address: "0x522B12eB0a9807b6d13717089124ce9420d495AC",
     nameVersion: "LockDealNFT@0.9.0"
+  },
+  vaultManager: {
+    address: "0xe8bdd7C33eA8783176Fd0C8Df7B255cF64fEEFda",
+    nameVersion: "VaultManager@0.0.4"
+  },
+  dealProvider: {
+    address: "0x5b007F06CB9708A1E419Fe936aE826C4eba21a8d",
+    nameVersion: "DealProvider@0.9.0"
+  },
+  lockDealProvider: {
+    address: "0x39f5487929e68CC0242905d1f6e97d0C0992cb77",
+    nameVersion: "LockDealProvider@0.9.0"
+  },
+  timedDealProvider: {
+    address: "0x3D0Ca2259046D6660937864b67287269268D2b61",
+    nameVersion: "TimedDealProvider@0.9.0"
+  },
+  collateralProvider: {
+    address: "0xE63De121ca8Fc1C540DBCd432d1c9D3A5cDEB94A",
+    nameVersion: "CollateralProvider@0.9.0"
+  },
+  refundProvider: {
+    address: "0x9a3188e32Bda4C47491186b7DBd412854A8e3921",
+    nameVersion: "RefundProvider@0.9.0"
+  },
+  simpleBuilder: {
+    address: "0x4338C2706052930C065CD7fe396f4E70494Cf7B3",
+    nameVersion: "SimpleBuilder@0.9.0"
+  },
+  simpleRefundBuilder: {
+    address: "0x9c39a03459522185a1598D4ad2a9cCCCB0F5Ff8f",
+    nameVersion: "SimpleRefundBuilder@0.9.0"
   }
 }
