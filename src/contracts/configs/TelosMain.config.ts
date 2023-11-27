4bf37363829ca41b17c4378b6075344d96c63939
Ashwin Arora <ashwinarora48@gmail.com>
2023-11-27 20:31:37 +0530

    Telos new chain added with multiSender and whitelist

---
+++ src/contracts/configs/TelosMain.config.ts
@@ -0,0 +1,9 @@
+import type { TChainConfig } from "../../types/IThePoolzInterface"
+
+export const NeonChainConfig: TChainConfig = {
+  multiSender: {
+    address: "0x7Ff9315f538dF7eC76Ec4815249Dd30519726460",
+    nameVersion: "MultiSender@1.0.0"
+  },
+  whiteListAddress: "0xb16BBDf683fFd6D92290F7610bb10f22f9c71e9e",
+}
