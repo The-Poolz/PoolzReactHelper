ea3407e0c36053083d2a0b4c943ccd710d3ec94f
Vitaliy <dynnik.vitaliy@gmail.com>
2023-12-19 15:35:40 +0200

    Issue #43 (#44)
    * Issue #43
    
    * Removed old token address

--- src/contracts/configs/MaticMain.config.ts
+++ src/contracts/configs/MaticMain.config.ts
@@ -7,5 +7,41 @@ export const MaticMainChainConfig: TChainConfig = {
     address: "0x9D13B213852669077131f8A24A676f27ab0C2931",
     nameVersion: "LockedDeal@2.3.2"
   },
-  poolzTokenAddress: "0x69A95185ee2a045CDC4bCd1b1Df10710395e4e23"
+
+  lockDealNFT: {
+    address: "0x9C36786836A594e3b355bA572A5Cd6841F69d86e",
+    nameVersion: "LockDealNFT@0.9.1-ironblocks"
+  },
+  vaultManager: {
+    address: "0x06fd710fD167f1f08b61e457F41D6e7c7DD9AF3D",
+    nameVersion: "VaultManager@0.0.4-ironblocks"
+  },
+  dealProvider: {
+    address: "0x0E85639403586c6c8B606AECB8A4b66be64f7A91",
+    nameVersion: "DealProvider@0.9.1-ironblocks"
+  },
+  lockDealProvider: {
+    address: "0x1FBeaEB2A2030787AF8026cE8C34E071CF4d8C5f",
+    nameVersion: "LockDealProvider@0.9.1-ironblocks"
+  },
+  timedDealProvider: {
+    address: "0x79Ab96f1705AdEa077a42026d77Ee945F802992c",
+    nameVersion: "TimedDealProvider@0.9.1-ironblocks"
+  },
+  collateralProvider: {
+    address: "0xd9f092eD4536E658A039019A7022C2Cff8BCB549",
+    nameVersion: "CollateralProvider@0.9.1-ironblocks"
+  },
+  refundProvider: {
+    address: "0xb6B0d332F3B4de249a0108bb58F0A60bb059d0Ee",
+    nameVersion: "RefundProvider@0.9.1-ironblocks"
+  },
+  simpleBuilder: {
+    address: "0x19DC2A2F214cC7538AF8f138637b21D150eE1030",
+    nameVersion: "SimpleBuilder@0.9.1-ironblocks"
+  },
+  simpleRefundBuilder: {
+    address: "0xaAA06C1b7D9Bd4577925a7535165e2F76E0f6969",
+    nameVersion: "SimpleRefundBuilder@0.9.1-ironblocks"
+  }
 }
