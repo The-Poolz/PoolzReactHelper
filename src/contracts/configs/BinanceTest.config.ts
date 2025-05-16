f35485cd1e32b04eed577c6e1369b83a9e16c9d6
Tai Nguyen <thanhtaipkl@gmail.com>
2025-05-16 18:00:29 +0700

    add contract for invest provider

--- src/contracts/configs/BinanceTest.config.ts
+++ src/contracts/configs/BinanceTest.config.ts
@@ -19,6 +19,7 @@ export const BinanceTestChainConfig: TChainConfig = {
       }
     ]
   },
+  investProviderAddress: "0x1Bebef1aBf49F9e588850b9Dc0Bf1AC6872C33D2",
   poolzAddress: "0xEEF0b7a6cd416006815ed3D66B670e1da73E116C",
   dispenserAddress: "0x7352116Fc2C7E7FF172D9E7dA2F31250E537ACcb",
   whiteListAddress: "0xC68CD044B5F888b8D3054C9dDD7a396b73f4418c",
